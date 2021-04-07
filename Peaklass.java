81bdd9f763e0f602c12351a3d600fdbfe7dd9b4a
uku20 <73695231+uku20@users.noreply.github.com>
2021-04-07 21:36:40 +0300

    Add files via upload

--- Peaklass.java
+++ Peaklass.java
@@ -26,30 +26,87 @@ public class Peaklass {
         //mängujuht.alusta();
         Mänguväli.väljastaMängulaud(kaardid);
 
-        while (õigeid!=mituPaari) {
+        int[][] indeksid = {{1,1},{1,2},{1,3},{1,4},
+                            {2,1},{2,2},{2,3},{2,4},
+                            {3,1},{3,2},{3,3},{3,4},
+                            {4,1},{4,2},{4,3},{4,4},{4,5},{1,1}};
+
+
+        //Ilma scanneriteta vundament, saab muutujaid vabalt väärtustada ja väljundeid lisada.
+        /**int mituPaari = (lauasuurus**2) / 2;
+        while (õigeid!=mituPaari){
+            if(x<lauasuurs && y<lauasuurus){
+                String täht1 = Mänguväli.valiKaks(kate, kaardid, x, y);
+                if(x2<lauasuurs && y2<lauasuurus){
+                    String täht2 = Mänguväli.valiKaks(kate, kaardid, x2, y2);
+                    if(!(x==x2 && y==y2)){
+                        if(täht1.equals(täht2)){
+                            kate[x][y] = kaardid[x][y];
+                            kate[x2][y2] = kaardid[x2][y2];
+                            õigeid += 1;
+                        }else{
+                            kate[x][y] = "#";
+                            kate[x2][y2] = "#";
+                        }
+                    }else{
+                        //midagi väljastab
+                    }
+                }else{
+                    //midagi väljastab
+                }
+            }else{
+                //midagi väljastab
+            }
+        }*/
+
+        while (õigeid!=mituPaari) { //koos skanneriga
             System.out.println();
             Mänguväli.väljastaMängulaud(kate);
             System.out.println();
             mängujuht.küsiKaarti();
             int x = mängujuht.getxKoordinaat();
             int y = mängujuht.getYkoordinaat();
-            String täht1 = Mänguväli.valiKaks(kate, kaardid, x, y);
-            mängujuht.küsiKaarti();
-            int x2 = mängujuht.getxKoordinaat();
-            int y2 = mängujuht.getYkoordinaat();
-            String täht2 = Mänguväli.valiKaks(kate, kaardid, x2, y2);
-            if(täht1.equals(täht2)){
-                kate[x][y] = kaardid[x][y];
-                kate[x2][y2] = kaardid[x2][y2];
-                System.out.println("õige");
-                õigeid += 1;
-            }else{
-                kate[x][y] = "#";
-                kate[x2][y2] = "#";
-                System.out.println("vale");
+            if(x<6 && y<6){                     //lauasuurustest väiksemad
+                String täht1 = Mänguväli.valiKaks(kate, kaardid, x, y);
+                mängujuht.küsiKaarti();
+                int x2 = mängujuht.getxKoordinaat();
+                int y2 = mängujuht.getYkoordinaat();
+                if(x2<6 && y2<6) {              //lauasuurustest väiksemad
+                    if (!(x2 == x && y2 == y)) {
+                        String täht2 = Mänguväli.valiKaks(kate, kaardid, x2, y2);
+                        if (täht1.equals(täht2)) {
+                            kate[x][y] = kaardid[x][y];
+                            kate[x2][y2] = kaardid[x2][y2];
+                            System.out.println("õige");
+                            õigeid += 1;
+                        } else {
+                            kate[x][y] = "#";
+                            kate[x2][y2] = "#";
+                            System.out.println("vale");
+                        }
+                    }else{
+                        System.out.println("võta uuesti");
+                    }
+                }else{
+                    System.out.println("võta uuesti");
+                }
             }
+            else{
+                System.out.println("võta uuesti");
+            }
+
         }
 
         //mängujuht.lõpeta();
+    }
+    public static void ootaSekundeid(int n){
+        try
+        {
+            Thread.sleep(n*1000);
+        }
+        catch(InterruptedException ex)
+        {
+            Thread.currentThread().interrupt();
+        }
     }
 }
